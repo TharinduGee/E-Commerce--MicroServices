2eded1e71f5e94d24fb06879e2a816e514e22600
Tharindu <tharindugeethanjan@gmail.com>
2024-05-10 00:23:20 +0530

    service discovery server + client config

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -38,7 +38,7 @@ public class OrderService {
 
         //call inventory service place order if product is in the stock
         InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
-                .uri("http:/inventory-service/api/inventory",
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()
                 )
                 .retrieve()
